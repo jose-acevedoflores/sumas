e7c685ec5f4cb58be0c16039b7e0ccd35484cc40
Jose Acevedo <acevedoflores.jose@gmail.com>
2020-12-31 12:09:54 -0400

    Adding overlay menu styles

--- src/OverlayedMenu/OverlayedMenu.js
+++ src/OverlayedMenu/OverlayedMenu.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import './overlayed-menu.css';
 
+const menuEntries = [
+    "Home",
+    "SuMAS Project",
+    "Our Team",
+    "Opportunities",
+    "Apply",
+    "Contact Us"
+];
+
+function buildLiEntry(entry) {
+    const isSelected = entry === "Home";
+    const clazz = "om-marker " + ( isSelected ? "om-li-selected" : "");
+    return (
+        <li key={entry}> 
+            <span> {entry} </span> <span className={clazz}/>
+        </li>
+    )
+}
 
 function OverlayedMenu(props) {
     
     const clazz = props.menuOpened ? "om-opened" : "om-closed";
     return (
         <div id="overlayed-menu" className={clazz}>
+            <ul id="om-list">
+                {menuEntries.map(buildLiEntry)}
+            </ul>
         </div>
     )
 }
